4104893c2cf703646fdd52e178174b18ad2a6d1f
Riya Ghosal <riya@Riyas-MacBook-Pro.local>
2023-04-11 17:24:48 +0530

    upgraded feature to vend denomination of 10 along with 5,2,1 now more structured

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -2,19 +2,16 @@ const determineNumberOfCoins = function(amount, denomination) {
   return Math.floor(amount / denomination);
 }
 
-const determineOptimumNumberOfCoins = function(amount) {
+const determineOptimumNumberOfCoins = function(rupees) {
+  const denominations = [10, 5, 2, 1]
+
+  let amount = rupees;
   let coins = 0;
+  for(let currentDenominations of denominations) {
 
-  coins = coins + determineNumberOfCoins(amount, 10);
-  amount = amount % 10;
-
-  coins = coins + determineNumberOfCoins(amount, 5);
-  amount = amount % 5;
-
-  coins = coins + determineNumberOfCoins(amount, 2);
-  amount = amount % 2;
-
-  coins = coins + determineNumberOfCoins(amount, 1);
+    coins = coins + determineNumberOfCoins(amount, currentDenominations);
+    amount = amount % currentDenominations;
+  }
 
   return coins;
 }
