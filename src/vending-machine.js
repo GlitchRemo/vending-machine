6d00527e1a5776223cff2d0655de82ebe5c8a99a
Riya Ghosal <riya@Riyas-MacBook-Pro.local>
2023-04-11 17:12:43 +0530

    upgraded feature to vend denomination of 10 along with 5,2,1

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,5 +1,22 @@
+const determineNumberOfCoins = function(amount, denomination) {
+  return Math.floor(amount / denomination);
+}
+
 const determineOptimumNumberOfCoins = function(amount) {
-  return Math.floor(amount / 2) + amount % 2;
+  let coins = 0;
+
+  coins = coins + determineNumberOfCoins(amount, 10);
+  amount = amount % 10;
+
+  coins = coins + determineNumberOfCoins(amount, 5);
+  amount = amount % 5;
+
+  coins = coins + determineNumberOfCoins(amount, 2);
+  amount = amount % 2;
+
+  coins = coins + determineNumberOfCoins(amount, 1);
+
+  return coins;
 }
 
 exports.determineOptimumNumberOfCoins = determineOptimumNumberOfCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -4,11 +4,13 @@ const assert = testing.assert;
 const determineOptimumNumberOfCoins = vendingMachine.determineOptimumNumberOfCoins;
 
 const runTest = function(){
-  assert(0,determineOptimumNumberOfCoins(0),"zero coin of Re.1 equals to zero rupee");
+  assert(0,determineOptimumNumberOfCoins(0),"zero coin equals to zero rupee");
   assert(1,determineOptimumNumberOfCoins(1),"one coin of Re.1 equals to one rupee");
   assert(2,determineOptimumNumberOfCoins(3),"one coin of Re.1 and one coin of Rs.2 equals to three rupees");
   assert(2,determineOptimumNumberOfCoins(4),"two coins of Rs.2 equals to four rupees");
-  assert(5,determineOptimumNumberOfCoins(10),"five coins of Rs.2 equals to ten rupees");
+  assert(1,determineOptimumNumberOfCoins(10),"one coin of Rs.10 equals to ten rupees");
+  assert(3,determineOptimumNumberOfCoins(13),"one coin of Rs.10, one coin of Rs.2 and one coin of Re.1 equals to thirteen rupees");
+  assert(3,determineOptimumNumberOfCoins(21),"two coins of Rs.20 and one coin of Re.1 equals to twenty one rupees");
 }
 
 runTest();
