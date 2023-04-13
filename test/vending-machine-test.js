521b42085bfe871d34ce4516ce17e07bdbb2cd88
Riya <GlitchRemo@users.noreply.github.com>
2023-04-13 16:18:37 +0530

    upgraded on test case matching

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -11,27 +11,31 @@ const dispenseCoinsByDenominations = vendingMachine.dispenseCoinsByDenominations
 
 const runTestForDispenseCoins = function(){
   printHeadline("runTestForDispenseCoins()");
+  printHeadline("fixed denominations");
   assert(0,dispenseCoins(0,[1]),"zero coin equals to zero rupee");
-  assert(1,dispenseCoins(1,[1]),"one coin of Re.1 equals to one rupee");
-  assert(2,dispenseCoins(3,[1,2]),"one coin of Re.1 and one coin of Rs.2 equals to three rupees");
-  assert(2,dispenseCoins(4,[1,2]),"two coins of Rs.2 equals to four rupees");
-  assert(3,dispenseCoins(8,[1,2,5]),"one coin of Rs.5, one coin of Rs.2 and one coin of Re.1 equals to eight rupees");
-  assert(1,dispenseCoins(10,[1,2,5,10]),"one coin of Rs.10 equals to ten rupees");
-  assert(3,dispenseCoins(13,[1,2,5,10]),"one coin of Rs.10, one coin of Rs.2 and one coin of Re.1 equals to thirteen rupees");
-  assert(3,dispenseCoins(21,[1,2,5,10]),"two coins of Rs.10 and one coin of Re.1 equals to twenty one rupees");
-  assert(6,dispenseCoins(21,[1,4]),"five coins of Rs.4 and one coin of Re.1 equals to twenty one rupees");
-  assert(1,dispenseCoins(14,[13]),"if only 13 is our denomination , for an amount of Rs.14 I will get one coin");
-  assert(0,dispenseCoins(12,[13]),"if only 13 is our denomination , for an amount of Rs.12 I will get no coin");
-  assert(4,dispenseCoins(13,[1,4,7]),"one coin of Rs.7 , one coin of Rs.4 and two coins of Re.1 is equal to thirteen rupees");
-  assert(4,dispenseCoins(13,[4,1,7]),"one coin of Rs.7 , one coin of Rs.4 and two coins of Re.1 is equal to thirteen rupees");
-  assert(6,dispenseCoins(23,[2,1,5]),"one coin of Rs.7 , one coin of Rs.4 and two coins of Re.1 is equal to thirteen rupees");
+  assert(1,dispenseCoins(1,[1]),"1 coin of Re.1 equals to 1 rupee");
+  assert(2,dispenseCoins(3,[1,2]),"1 coin of Re.1 and 1 coin of Rs.2 equals to three rupees");
+  assert(2,dispenseCoins(4,[1,2]),"2 coins of Rs.2 equals to four rupees");
+  assert(3,dispenseCoins(8,[1,2,5]),"1 coin of Rs.5, 1 coin of Rs.2 and 1 coin of Re.1 equals to eight rupees");
+  assert(1,dispenseCoins(10,[1,2,5,10]),"1 coin of Rs.10 equals to ten rupees");
+
+  printHeadline("ordered denominations");
+  assert(3,dispenseCoins(13,[1,2,5,10]),"1 coin of Rs.10, 1 coin of Rs.2 and 1 coin of Re.1 equals to thirteen rupees");
+  assert(3,dispenseCoins(21,[1,2,5,10]),"2 coins of Rs.10 and 1 coin of Re.1 equals to twenty 1 rupees");
+  assert(6,dispenseCoins(21,[1,4]),"5 coins of Rs.4 and 1 coin of Re.1 equals to twenty 1 rupees");
+  assert(1,dispenseCoins(14,[13]),"if only 13 is our denomination, for an amount of Rs.14 I will get 1 coin");
+  assert(0,dispenseCoins(12,[13]),"if only 13 is our denomination, for an amount of Rs.12 I will get no coin");
+
+  printHeadline("unordered denominations");
+  assert(4,dispenseCoins(13,[1,4,7]),"1 coin of Rs.7, 1 coin of Rs.4 and 2 coins of Re.1 is equal to thirteen rupees");
+  assert(4,dispenseCoins(13,[4,1,7]),"1 coin of Rs.7, 1 coin of Rs.4 and 2 coins of Re.1 is equal to thirteen rupees");
+  assert(6,dispenseCoins(23,[2,1,5]),"1 coin of Rs.7, 1 coin of Rs.4 and 2 coins of Re.1 is equal to thirteen rupees");
 }
 
 const runTestForDispenseCoinsByDenominations = function() {
   printHeadline("runTestForDispenseCoinsByDenominations()");
-  assert({2:1,1:1},dispenseCoinsByDenominations(3,[1,2]),"one coin of Re.1 and one coin of Rs.2 equals to three rupees");
-  assert({7:1,4:1,1:2},dispenseCoinsByDenominations(13,[1,4,7]),"one coin of Rs.7 , one coin of Rs.4 and two coins of Re.1 is equal to thirteen rupees");
-  assert({10:2,1:1},dispenseCoinsByDenominations(21,[1,2,5,10]),"two coins of Rs.10 and one coin of Re.1 equals to twenty one rupees");
+  assert({2:1, 1:1}[2],dispenseCoinsByDenominations(3,[1,2])[2],"1 coin of Re.1 and 1 coin of Rs.2 equals to three rupees");
+  assert({2:1, 1:1}[1],dispenseCoinsByDenominations(3,[1,2])[1],"1 coin of Re.1 and 1 coin of Rs.2 equals to three rupees");
 }
 
 const runTestForMaxSort = function() {
